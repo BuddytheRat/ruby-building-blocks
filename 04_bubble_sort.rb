cdc12afcc68b842eabc4414a03518c229a3f6094
Nathan <BuddytheRat@gmail.com>
2015-06-22 13:27:15 -0500

    bubble_sort_by working

--- 04_bubble_sort.rb
+++ 04_bubble_sort.rb
@@ -1,26 +1,27 @@
-def bubble_sort(array)
-	new_array = array
+def bubble_sort_by(array)
 	sorts = 0
-	new_array.size.times do
+	array.size.times do
 		sorts += 1
-		left_to_sort = new_array.size - sorts
+		left_to_sort = array.size - sorts
 		left_to_sort.times do |i|
-			if new_array[i] > new_array[i+1]
-				new_array[i], new_array[i+1] = new_array[i+1], new_array[i]
+			compare = yield(array[i], array[i+1])
+			if compare < 0
+				array[i], array[i+1] = array[i+1], array[i]
 			end
 		end
 	end
-	new_array
+	array
+end
+
+def bubble_sort(array)
+	bubble_sort_by(array) { |x, y| y - x }
 end
 
 puts bubble_sort([4, 3, 78, 2, 0, 2])
 #=> [0, 2, 2, 3, 4, 78]
 
-def bubble_sort_by(array)
-	
-end
-
-bubble_sort_by(["hi", "hello", "hey"]) do |left, right|
+sorted = bubble_sort_by(["hi", "hello", "hey"]) do |left, right|
 	right.length - left.length
 end
+puts sorted
 #=> ["hi", "hey", "hello"]
